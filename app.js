388ea9a87a995c3839b1c771a246ea89799a2ce8
Renal Apriansyah <55680360+reyaprtiket@users.noreply.github.com>
2021-06-24 11:48:57 +0700

    connect to postgresql

--- app.js
+++ app.js
@@ -2,9 +2,19 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const sequelize = require('./src/config/postgresql');
 const healthCheckRouter = require('./src/routes/index');
 
 const port = process.env.PORT || 3000;
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
 
 var app = express();
 
